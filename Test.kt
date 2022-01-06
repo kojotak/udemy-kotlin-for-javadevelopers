1f6a00061a9af318cacfdf77d3f7c91caf73ef9f
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-06 21:43:52 +0100

    chapter 1

---
+++ Test.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+    area(height=12, width=10 ) //named parameters, can change position
+}
+
+fun area(width: Int, height: Int){
+    println("width=$width and height= $height")
+}
