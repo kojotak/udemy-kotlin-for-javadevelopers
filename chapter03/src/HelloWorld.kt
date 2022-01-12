bb3e622489f271cba8996f6ea80e1151587ab768
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-12 20:56:03 +0100

    kotlin std library

---
+++ chapter03/src/HelloWorld.kt
@@ -0,0 +1,12 @@
+//zadne stredniky, kotlin si dotahne standardni kotlini knihovny sam (vcetne rozsireni java knihoven)
+
+//nepotrebuji tridu
+fun main(args: Array<String>) {
+    //Array je trida, jako vsechno v Kotlinu
+    //nemusime deklarovat, ze funkce vraci void
+    //je to "top level function", protoze neni ve tride
+
+    println("Hello world")
+    //zadne stredniky
+
+}
