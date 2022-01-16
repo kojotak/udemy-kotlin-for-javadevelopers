22b179ee19c3c6a8bbfdecb3a1d40d3de2a2a08f
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-16 18:19:13 +0100

    ch05 - classes - constructors (primary constructors are optional)

--- chapter05/src/Classes.kt
+++ chapter05/src/Classes.kt
@@ -10,8 +10,18 @@ fun main(args: Array<String>) {
     val emp3 = Employee("Jane")
     println(emp3.firstName)
     println(emp3.fullTime)
+
+    println(Demo().dummy)
 }
 
 class Employee (val firstName: String, var fullTime: Boolean = true){
 
+}
+
+class Demo {//we don't have to declare primary constructor
+    val dummy: String
+
+    constructor(){
+        dummy = "Hello"
+    }
 }
