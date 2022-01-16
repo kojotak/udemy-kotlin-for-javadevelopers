79bd4d7c107c11e0d5eddc641fa970cf26ebcf14
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-16 18:16:39 +0100

    ch05 - classes - constructors (primary & secondary)

--- chapter05/src/Classes.kt
+++ chapter05/src/Classes.kt
@@ -2,10 +2,25 @@
 fun main(args: Array<String>) {
     val emp = Employee("John")
     println(emp)
+
+    val emp2 = Employee("Joe")
+    println(emp2.firstName)
+    println(emp2.fullTime)
+
+    val emp3 = Employee("Jane")
+    println(emp3.firstName)
+    println(emp3.fullTime)
 }
 
-class Employee (firstName: String){ //can remove the constructor keyword - it will generate public one
+class Employee (val firstName: String){ //primary constructor
 
-    //Kotlin will declare firstName property for us
+    //have to declare properties from secondary constructor manually
+    var fullTime : Boolean = true
+
+    //this is the secondary constructor
+    constructor(firstName: String, fullTime: Boolean) : this(firstName){
+        // this(firstName) calls the primary constructor
+        // = same situation as in JavaEmployee
+    }
 
 }
--- chapter05/src/JavaEmployee.java
+++ chapter05/src/JavaEmployee.java
@@ -1,7 +1,14 @@
 public class JavaEmployee {
 
     private final String firstName;
+    private final boolean fullTime;
+
     public JavaEmployee(String firstName){
+        this(firstName, true);
+    }
+
+    public  JavaEmployee(String firstName, boolean fullTime){
         this.firstName = firstName;
+        this.fullTime = fullTime;
     }
 }
