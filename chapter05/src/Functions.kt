dc2cf17d32cc6a5acfacc7e5708f4ff71d588a9a
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-17 21:22:48 +0100

    ch05 - functions - varargs

--- chapter05/src/Functions.kt
+++ chapter05/src/Functions.kt
@@ -4,13 +4,18 @@ fun main(args: Array<String>) : Unit {
     println(labelMultiply(3,4,"The result is: "))
     println(labelMultiply(5,4))
 
-    //can change the order of parameters
+    //can change the order of named parameters
     println(labelMultiply(label="Foo", operand2=2, operand1=1))
 
     val emp = FunEmployee("Jane")
     println("emp: ${emp.upperCaseFirstName()}") //JANE
 
+    val car1 = Car("blue","Toyota", 2015)
+    val car2 = Car("red", "Mazda", 2011)
+    val car3 = Car("gray", "Ford", 2012)
 
+    printColors(car1, car2, car3, str = "message")
+    //kdyz by vararg byl posledni parametr, nemusel bych pouzit named prameter pro urceni, co je string
 }
 
 //function with expression body
@@ -23,4 +28,13 @@ class FunEmployee(val firstName: String){
     fun upperCaseFirstName() = firstName.toUpperCase()
 }
 
+data class FunCar(val color: String, val model: String, val yer: Int){
+
+}
+
+fun printColors(vararg cars: Car, str: String){
+    for(car in cars){
+        println("car color ${car.color}")
+    }
+}
 
