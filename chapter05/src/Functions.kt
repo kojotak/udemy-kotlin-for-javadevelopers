e7507474c26dde2d68381325f8bedba549575864
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-17 21:16:01 +0100

    ch05 - functions - added default value for parameter

--- chapter05/src/Functions.kt
+++ chapter05/src/Functions.kt
@@ -1,8 +1,23 @@
 
-//default return type is Unit
+//default return type is Unit (nothing)
 fun main(args: Array<String>) : Unit {
     println(labelMultiply(3,4,"The result is: "))
+    println(labelMultiply(5,4))
+
+    val emp = FunEmployee("Jane")
+    println("emp: ${emp.upperCaseFirstName()}") //JANE
+
+
 }
 
-fun labelMultiply(operand1: Int, operand2: Int, label : String)  =
+//function with expression body
+fun labelMultiply(operand1: Int, operand2: Int, label : String = "The answer is")  =
      ("$label ${operand1 * operand2}")
+
+fun whatever() = 3*4
+
+class FunEmployee(val firstName: String){
+    fun upperCaseFirstName() = firstName.toUpperCase()
+}
+
+
