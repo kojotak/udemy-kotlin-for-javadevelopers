4347abcc6faadc134d6d25291540568942819934
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-17 21:37:08 +0100

    ch05 - extension function - simplification

--- chapter05/src/Functions.kt
+++ chapter05/src/Functions.kt
@@ -61,7 +61,8 @@ fun printColors(vararg cars: Car, str: String){
 
 //extension function to String
 //there is no parameter... we are using this reference to the string itself
+//update - we can remove the this reference
 fun String.upperFirstAndLast(): String {
-    val upperFirst = this.substring(0,1).toUpperCase() + this.substring(1)
+    val upperFirst = substring(0,1).toUpperCase() + substring(1)
     return upperFirst.substring(0, upperFirst.length-1) + upperFirst.substring(upperFirst.length-1, upperFirst.length).toUpperCase()
 }
