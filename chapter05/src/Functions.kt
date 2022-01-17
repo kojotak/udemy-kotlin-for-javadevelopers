3c8f2990d1c516b24b7b2591021f1f6d177d053d
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-17 21:11:41 +0100

    ch05 - functions - further simplified function (complier will figure out the return type)

--- chapter05/src/Functions.kt
+++ chapter05/src/Functions.kt
@@ -4,5 +4,5 @@ fun main(args: Array<String>) : Unit {
     println(labelMultiply(3,4,"The result is: "))
 }
 
-fun labelMultiply(operand1: Int, operand2: Int, label : String) : String =
+fun labelMultiply(operand1: Int, operand2: Int, label : String)  =
      ("$label ${operand1 * operand2}")
