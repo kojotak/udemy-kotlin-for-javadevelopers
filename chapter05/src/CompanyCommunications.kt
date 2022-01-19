9da38072dadb77cec6fd53f1556050d99cfc9563
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-19 21:28:14 +0100

    ch05 - companion object (static access)

--- chapter05/src/CompanyCommunications.kt
+++ chapter05/src/CompanyCommunications.kt
@@ -3,6 +3,7 @@ import java.time.Year
 fun main(ars: Array<String>){
     println(CompanyCommunications.getTagLine())
     println(CompanyCommunications.getCopyrightLine())
+    println(SomeClass.SOME.accessPrivateVar())
 }
 
 //no class keyword here
@@ -16,4 +17,17 @@ object CompanyCommunications {
     fun getTagLine() = "Our company rocks!"
 
     fun getCopyrightLine() = "Copyright $currentYear blah blah"
+}
+
+class SomeClass {
+
+    //how to access "equivalent java static" properties?
+    companion object SOME {//SOME is optional name, the default is "Companion"
+        //everything in companion object is "static"
+        private val privateVar = 6
+        fun accessPrivateVar(){
+            println("I'm accessing privateVar:  + $privateVar")
+        }
+    }
+
 }
