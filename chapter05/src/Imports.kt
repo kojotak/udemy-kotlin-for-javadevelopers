f457e75bb6a1f2bee01b28ee44b28413412d4713
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-22 10:44:33 +0100

    ch05 - imports

---
+++ chapter05/src/Imports.kt
@@ -0,0 +1,5 @@
+package foo.bar.baz //it doesn't have to match directory structure...
+
+fun main(args:Array<String>){
+    println("my package doesn't match!")
+}
---
+++ chapter05/src/anotherpackage/anotherfile.kt
@@ -0,0 +1,7 @@
+package anotherpackage
+
+import whatever //imports a file
+
+fun main(args:Array<String>){
+    println("using function from other package: ${whatever()}")
+}
