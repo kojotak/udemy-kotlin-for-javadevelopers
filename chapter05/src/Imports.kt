e783a06602071ad9997113225e4be147feb125e4
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-22 10:59:08 +0100

    ch05 - imports and modules

--- chapter05/src/Imports.kt
+++ chapter05/src/Imports.kt
@@ -1,5 +1,11 @@
-package foo.bar.baz //it doesn't have to match directory structure...
+//it doesn't have to match directory structure...
+package foo.bar.baz
+
+import chapter02.LocationKotlin //have to add dependency to another module in chater05.iml
+
 
 fun main(args:Array<String>){
     println("my package doesn't match!")
+
+    println(LocationKotlin(42, "using class from other module"))
 }
--- chapter05/src/anotherpackage/anotherfile.kt
+++ chapter05/src/anotherpackage/anotherfile.kt
@@ -1,7 +1,14 @@
 package anotherpackage
 
 import whatever //imports a file
+import upperFirstAndLast //imports a function
 
 fun main(args:Array<String>){
     println("using function from other package: ${whatever()}")
+
+    println(CompanyCommunications.getCopyrightLine())
+
+    println(Department.IT.getInfo())
+
+    println("a string to work with".upperFirstAndLast())
 }
