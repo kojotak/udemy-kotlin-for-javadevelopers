f1609ca45bb8945145829401a2d8f7b16d403eb2
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-23 12:38:37 +0100

    ch05 - for loops

--- chapter06/src/Loops.kt
+++ chapter06/src/Loops.kt
@@ -23,5 +23,42 @@ fun main(args: Array<String>){
     val stepThree = stepRange.step(3) //only for numeric ranges
     val reversedRange = range.reversed()
 
-
+    println("for loops...")
+    for(i in range){
+        print("$i ") //1 2 3 4 5
+    }
+    println()
+    for(n in stepRange){
+        print("$n ") //3 4 5 6 ... 14 15
+    }
+    println()
+    for(n in stepThree){
+        print("$n ") //3 6 9 12 15
+    }
+    println()
+    for(n in reversedRange){
+        print("$n ") //5 4 3 2 1
+    }
+    for(i in 1..3){
+        print("$i ") //shorthand: 1 2 3
+    }
+    println("iterating strings...")
+    //for(s in stringRange) //nejde, pro stringy potrebujeme iterator
+    for(c in "hello world"){//muzeme ale for-cyklovat string
+        print("$c ") //h e l l o  w o r l d
+    }
+    println()
+    for(num in 1..10 step 4){
+        print("$num ")//1 5 9 (skacu po 4 od 1 do 10)
+    }
+    println()
+    for(i in 20 downTo 10 step 5){
+        print("$i ")//20 15 10
+    }
+    println()
+    //kdyz chci neco preskocit/vynechat
+    for(i in 1 until 10){
+        print("$i ") //1 2 ... 8 9 - bez 10
+    }
+    println()
 }
