905b7b9168404c345ea6cc47d09c7ade2fea95a9
Tomáš Beneda <tomas.beneda@gmail.com>
2022-01-31 21:02:08 +0100

    chapter 07 - more on lists

--- chapter07/src/Lists.kt
+++ chapter07/src/Lists.kt
@@ -19,10 +19,28 @@ fun main(ars: Array<String>){
     val mutableList = mutableListOf<Int>(1,2,3)
     println(mutableList)//[1, 2, 3]
 
-    val array = arrayOf("black","white","green")
+    val array = arrayOf("black","white","green","blue")
     val colorList = listOf(array)
     println(colorList)//[[Ljava.lang.String;@70177ecd] - zabaleny pole
 
     val ints = intArrayOf(1,2,3)
     println(ints.toList())//[1, 2, 3]
+
+    println(strings.asReversed())
+//    if(strings.size > 5){
+//        println(strings[5])
+//    }
+    println(strings.getOrNull(5)) //null
+
+    println(array.zip(strings))//creates pair elements: [(black, spring), (white, summer), (green, fall), (blue, winter)]
+
+    val mergedLists = listOf(colorList, strings)
+    println(mergedLists)//[[[Ljava.lang.String;@70177ecd], [spring, summer, fall, winter]]
+
+    val combinedList = colorList + strings
+    println(combinedList)//[[Ljava.lang.String;@70177ecd, spring, summer, fall, winter]
+
+    println(strings.union(strings))//[spring, summer, fall, winter]
+
+    println(arrayOf("a","a","a","a").distinct())//odstrani duplicity [a]
 }
