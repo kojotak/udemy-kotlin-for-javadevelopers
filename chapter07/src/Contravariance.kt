dd83b69b6b912bab055480963f6e8377f4853a06
Tomáš Beneda <tomas.beneda@gmail.com>
2022-02-03 21:38:37 +0100

    chapter 07 - generics contravariance - finished

--- chapter07/src/Contravariance.kt
+++ chapter07/src/Contravariance.kt
@@ -1,7 +1,7 @@
 fun main(ars: Array<String>){
 
     val flowerTender = object: FlowerCare<CoFlower>{
-        override fun prune(flower: CoFlower) = println("prune a flower $flower")
+        override fun prune(flower: CoFlower) = println("prune a flower ${flower.name}")
     }
 
     val roseTender = object: FlowerCare<CoRose>{
@@ -16,7 +16,12 @@ fun main(ars: Array<String>){
     val daffodilGarden = CoGarden<Daffodil>(listOf(Daffodil(), Daffodil(),Daffodil()), daffodilTender)
     daffodilGarden.tendFlower(1)//prune a daffodil Daffodil@30dae81
 
+    //until now, we have used: interface FlowerCare<in T>{
     //we can not use one flowerTender and use it in roseGarden and in daffodilGarden
+    //until FlowerCare is contravariant, that is with relaxed type parameter
+
+    val roseGardenContravariant = CoGarden<CoRose>(listOf(CoRose(), CoRose(), CoRose()), flowerTender)
+    roseGardenContravariant.tendFlower(0)//prune a flower Rose
 }
 
 
@@ -29,15 +34,19 @@ class CoGarden<T: CoFlower>(val flowers:List<T>,
     }
 }
 
-open class CoFlower{
+open class CoFlower(val name: String){
 
 }
-class CoRose: CoFlower(){
+class CoRose: CoFlower("Rose"){
 
 }
-class Daffodil: CoFlower(){
+class Daffodil: CoFlower("Daffodil"){
 
 }
-interface FlowerCare<T>{
+//input type parameter for contravariance
+interface FlowerCare<in T>{
     fun prune(flower: T)
+
+    //if type parameter T is only 'in', we can not use it as return parameter
+    //fun pick(): T//Type parameter T is declared as 'in' but occurs in 'out' position in type T
 }
